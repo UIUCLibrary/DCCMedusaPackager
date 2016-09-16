387a7beccb847d94f31a921cd99f1af9ffa04803
hborcher <hborcher@illinois.edu>
2016-09-16 15:55:32 -0500

    added test to packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='MedusaPackager',
     version='0.0.1',
-    packages=['MedusaPackager', 'scripts'],
+    packages=['MedusaPackager', 'scripts', 'tests'],
     entry_points={'console_scripts': ['packagemedusa = scripts.process:main']},
     url='',
     license='',
