e0b882c948e076816dcb56bce5668920de6f14d0
Henry Borchers <henryborchers@yahoo.com>
2016-10-13 16:26:19 -0500

    Added scipts and set zipsafe to false

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='MedusaPackager',
+    name='DCCMedusaPackager',
     version='0.0.1',
     packages=['MedusaPackager', 'scripts', 'tests'],
     entry_points={
-        'console_scripts': ['packagemedusa = scripts.process:main']
+        'console_scripts': ['packagemedusa=process:main']
     },
     url='https://github.com/UIUCLibrary/DCCMedusaPackager',
+    scripts=['scripts/processcli.py'],
+    zip_safe=False,
     test_suite='tests',
     license='',
     author='Henry Borchers',
