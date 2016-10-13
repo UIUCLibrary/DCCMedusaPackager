e8d57179096e953b87e4fffb7e241139f3a57f97
Henry Borchers <henryborchers@yahoo.com>
2016-10-13 10:57:30 -0500

    Formatting and added description.

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ setup(
     name='MedusaPackager',
     version='0.0.1',
     packages=['MedusaPackager', 'scripts', 'tests'],
-    entry_points={'console_scripts': ['packagemedusa = scripts.process:main']},
-    url='',
+    entry_points={
+        'console_scripts': ['packagemedusa = scripts.process:main']
+    },
+    url='https://github.com/UIUCLibrary/DCCMedusaPackager',
     license='',
     author='Henry Borchers',
     author_email='hborcher@illinois.edu',
-    description=''
+    description='Script for packaging DCC files for ingesting into Medusa'
 )
