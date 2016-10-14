e3073677a7e501490dce1320f01187f40fecf53a
Henry Borchers <henryborchers@yahoo.com>
2016-10-14 10:53:21 -0500

    restored the missing lines of code

--- publishDocs.sh
+++ publishDocs.sh
@@ -7,6 +7,18 @@ make clean
 make html
 cd ..
 
-git add -A
+git add docs/build/html
 git commit -m "Building and publishing docs"
 git push origin master
+
+git checkout gh-pages
+rm -rf .
+touch .nojekyll
+git checkout master docs/build/html
+mv ./docs/build/html/* ./
+rm -rf ./docs
+git add -A
+git commit -m "publishing updated docs..."
+git push origin gh-pages
+# switch back
+git checkout master
