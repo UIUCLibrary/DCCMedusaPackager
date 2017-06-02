3f12d17de957f15cf7b7d78e6b75a87683e80795
hborcher <hborcher@illinois.edu>
2017-06-02 10:09:25 -0500

    init commit

---
+++ cx_setup.py
@@ -0,0 +1,22 @@
+from cx_Freeze import setup, Executable
+import MedusaPackager
+import platform
+
+EXECUTABLE_NAME = "packagemedusa"
+
+setup(
+    name=MedusaPackager.__title__,
+    version=MedusaPackager.__version__,
+    packages=[
+        'MedusaPackager',
+    ],
+    url=MedusaPackager.__url__,
+    description=MedusaPackager.__description__,
+    executables={
+        Executable("MedusaPackager/processcli.py",
+                   targetName=("{}.exe".format(EXECUTABLE_NAME) if platform.system() == "Windows" else EXECUTABLE_NAME))
+
+    },
+    # options={"build_exe": {'includes': includes}},
+
+)
