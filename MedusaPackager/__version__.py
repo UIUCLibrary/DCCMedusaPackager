69b61bb09e99b6f01d37045cdf4e249b660adff0
hborcher <hborcher@illinois.edu>
2017-06-09 14:51:54 -0500

    added FULL_TITLE

--- MedusaPackager/__version__.py
+++ MedusaPackager/__version__.py
@@ -1,6 +1,7 @@
 __title__ = "DCCMedusaPackager"
 __url__ = 'https://github.com/UIUCLibrary/DCCMedusaPackager'
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 __author__ = "Henry Borchers"
 __author_email__ = 'hborcher@illinois.edu'
 __description__ = "Script for packaging DCC files for ingesting into Medusa"
+FULL_TITLE = "DCC Medusa Packager"
