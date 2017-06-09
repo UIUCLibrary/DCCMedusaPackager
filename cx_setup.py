3f728121d67cb39599fc6bf7597b80b267f3740e
hborcher <hborcher@illinois.edu>
2017-06-09 14:51:11 -0500

    added shortcut to documentation

--- cx_setup.py
+++ cx_setup.py
@@ -2,7 +2,47 @@ from cx_Freeze import setup, Executable
 import MedusaPackager
 import platform
 
+
+def create_msi_tablename(python_name, fullname):
+    shortname = python_name[:6].replace("_", "").upper()
+    longname = fullname
+    return "{}|{}".format(shortname, longname)
+
+
 EXECUTABLE_NAME = "packagemedusa"
+
+directory_table = [
+    (
+        "ProgramMenuFolder",  # Directory
+        "TARGETDIR",  # Directory_parent
+        "PMenu|Programs",  # DefaultDir
+    ),
+    (
+        "PMenu",  # Directory
+        "ProgramMenuFolder",  # Directory_parent
+        create_msi_tablename(MedusaPackager.__title__, MedusaPackager.FULL_TITLE)
+    ),
+]
+shortcut_table = [
+    (
+        "startmenuShortcutDoc",  # Shortcut
+        "PMenu",  # Directory_
+        "{} Documentation".format(create_msi_tablename(MedusaPackager.__title__, MedusaPackager.FULL_TITLE)),
+        "TARGETDIR",  # Component_
+        "[TARGETDIR]documentation.url",  # Target
+        None,  # Arguments
+        None,  # Description
+        None,  # Hotkey
+        None,  # Icon
+        None,  # IconIndex
+        None,  # ShowCmd
+        'TARGETDIR'  # WkDir
+    ),
+]
+
+INCLUDE_FILES = [
+    "documentation.url"
+]
 
 setup(
     name=MedusaPackager.__title__,
@@ -19,7 +59,7 @@ setup(
     },
     options={"build_exe": {
         "include_msvcr": True
-        }
+    }
     },
 
 )
