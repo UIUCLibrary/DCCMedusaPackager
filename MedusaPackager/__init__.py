233d2837fc668c32e77bf3f86a651293b109a289
hborcher <hborcher@illinois.edu>
2017-06-09 15:21:48 -0500

    refactored for better reading

--- MedusaPackager/__init__.py
+++ MedusaPackager/__init__.py
@@ -1,2 +1,3 @@
 from .MedusaPackager import MedusaPackageData, find_package_files, dash_grouper, create_empty_package
-from .__version__ import __title__, __url__, __version__, __author__, __author_email__, __description__, FULL_TITLE
+from .__version__ import __title__, __url__, __version__, FULL_TITLE
+from .__version__ import __author__, __author_email__, __description__, __credits__
