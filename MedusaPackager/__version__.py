b70b06fafb94d3a3f74e86ef3ccbf7ce7c6ce9d2
hborcher <hborcher@illinois.edu>
2017-06-09 15:21:10 -0500

    changed the author to UIUC

--- MedusaPackager/__version__.py
+++ MedusaPackager/__version__.py
@@ -1,7 +1,8 @@
 __title__ = "DCCMedusaPackager"
 __url__ = 'https://github.com/UIUCLibrary/DCCMedusaPackager'
 __version__ = "0.0.6"
-__author__ = "Henry Borchers"
+__author__ = "University of Illinois at Urbana Champaign"
+__credits__ = "Henry Borchers"
 __author_email__ = 'hborcher@illinois.edu'
 __description__ = "Script for packaging DCC files for ingesting into Medusa"
 FULL_TITLE = "DCC Medusa Packager"
