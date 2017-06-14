f93367dd32fb9fb8b4963c948d1b9726eaa7ca3d
hborcher <hborcher@illinois.edu>
2017-06-14 15:16:05 -0500

    upped version

--- MedusaPackager/__version__.py
+++ MedusaPackager/__version__.py
@@ -1,6 +1,6 @@
 __title__ = "DCCMedusaPackager"
 __url__ = 'https://github.com/UIUCLibrary/DCCMedusaPackager'
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 __author__ = "University of Illinois at Urbana Champaign"
 __credits__ = "Henry Borchers"
 __author_email__ = 'hborcher@illinois.edu'
