3ff5965b2763906e7d1f10f863343ab769bc3f68
hborcher <hborcher@illinois.edu>
2017-12-20 15:52:51 -0600

    moved project metadata into setup.cfg

--- setup.py
+++ setup.py
@@ -1,17 +1,8 @@
 from setuptools import setup
-import MedusaPackager
 setup(
-    name=MedusaPackager.__title__,
-    version=MedusaPackager.__version__,
     packages=['MedusaPackager'],
     entry_points={
         'console_scripts': ['packagemedusa=MedusaPackager.processcli:main']
     },
-    url=MedusaPackager.__version__,
-    zip_safe=False,
     test_suite='tests',
-    license='',
-    author=MedusaPackager.__author__,
-    author_email=MedusaPackager.__author_email__,
-    description=MedusaPackager.__description__,
 )
